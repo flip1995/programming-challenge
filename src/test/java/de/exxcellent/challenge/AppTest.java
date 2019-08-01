ad355fc318bdf357ce2336dee0d5e241edab29cc
flip1995 <hello@philkrones.com>
2019-08-02 00:12:01 +0200

    Cleanup tests

--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -1,9 +1,12 @@
 package de.exxcellent.challenge;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Iterator;
 
 import de.exxcellent.challenge.data_classes.Football;
@@ -19,45 +22,44 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  */
 public class AppTest {
 
-    private Weather weather_low;
-    private Weather weather_high;
+    private static final ByteArrayOutputStream OUT = new ByteArrayOutputStream();
+    private static final PrintStream ORIGINAL_OUT = System.out;
 
-    private Football football_low;
-    private Football football_high;
-
-    @BeforeEach
-    public void setUp() {
-        this.weather_low = new Weather();
-        this.weather_low.setDay(2);
-        this.weather_low.setMax_temp(3);
-        this.weather_low.setMin_temp(1);
-
-        this.weather_high = new Weather();
-        this.weather_high.setDay(1);
-        this.weather_high.setMax_temp(10);
-        this.weather_high.setMin_temp(0);
-
-        this.football_low = new Football();
-        this.football_low.setTeam("Loser");
-        this.football_low.setGoals(3);
-        this.football_low.setGoals_allowed(10); // -7
-
-        this.football_high = new Football();
-        this.football_high.setTeam("Winner");
-        this.football_high.setGoals(3);
-        this.football_high.setGoals_allowed(0); // +3
+    @BeforeAll
+    public static void setOut() {
+        System.setOut(new PrintStream(OUT));
     }
 
     @Test
     public void weatherCmp() {
-        assertTrue(this.weather_low.cmp_diff(this.weather_high) < 0);
+        Weather weather_low = new Weather();
+        weather_low.setDay(2);
+        weather_low.setMax_temp(3);
+        weather_low.setMin_temp(1);
+
+        Weather weather_high = new Weather();
+        weather_high.setDay(1);
+        weather_high.setMax_temp(10);
+        weather_high.setMin_temp(0);
+
+        assertTrue(weather_low.cmp_diff(weather_high) < 0);
     }
 
     @Test
     public void footballCmp() {
-        assertTrue(this.football_low.cmp_diff(this.football_high) > 0);
-        this.football_low.setGoals(7); // diff -3 == +3 = diff football_high
-        assertEquals(this.football_low.cmp_diff(this.football_high), 0);
+        Football football_low = new Football();
+        football_low.setTeam("Loser");
+        football_low.setGoals(3);
+        football_low.setGoals_allowed(10); // -7
+
+        Football football_high = new Football();
+        football_high.setTeam("Winner");
+        football_high.setGoals(3);
+        football_high.setGoals_allowed(0); // +3
+
+        assertTrue(football_low.cmp_diff(football_high) > 0);
+        football_low.setGoals(7); // diff -3 == +3 = diff football_high
+        assertEquals(football_low.cmp_diff(football_high), 0);
     }
 
     @Test
@@ -81,6 +83,15 @@ public class AppTest {
     @Test
     public void runFootball() {
         App.main("");
+
+        Iterator<String> output = OUT.toString().lines().iterator();
+        assertEquals(output.next(), "Day with smallest temperature spread : 14");
+        assertEquals(output.next(), "Team with smallest goal spread       : Aston_Villa");
+    }
+
+    @AfterAll
+    public static void resetOut() {
+        System.setOut(ORIGINAL_OUT);
     }
 
 }
