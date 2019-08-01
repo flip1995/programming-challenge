abfe8b96bb413aef21fc7ac728b6ef088d8d548d
flip1995 <hello@philkrones.com>
2019-08-01 23:16:30 +0200

    Implement SmallestDiff Interface for DataContainer

--- src/main/java/de/exxcellent/challenge/DataContainer.java
+++ src/main/java/de/exxcellent/challenge/DataContainer.java
@@ -1,19 +1,30 @@
 package de.exxcellent.challenge;
 
+import java.io.IOException;
 import java.util.Iterator;
 
-public class DataContainer<T> {
+import de.exxcellent.challenge.queries.Diff;
+import de.exxcellent.challenge.queries.SmallestDiff;
+
+public class DataContainer<T extends Diff<T>> implements SmallestDiff<T> {
     private Iterator<T> data;
 
     public DataContainer(Iterator<T> data) {
         this.data = data;
     }
 
-    public boolean hasNext() {
-        return this.data.hasNext();
-    }
+    @Override
+    public T smallest_diff() throws IOException {
+        T min = this.data.next();
+        if (min == null)
+            throw new IOException("No data in CSV file");
+        while (this.data.hasNext()) {
+            T next = this.data.next();
 
-    public T next() {
-        return this.data.next();
-    }
+            if (next.cmp_diff(min) < 0)
+                min = next;
+        }
+
+        return min;        
+	}
 }
